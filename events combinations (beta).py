3da6de697a4852ace36598c009f4d2c646395712
melancholyguy <125413125+melancholyguy@users.noreply.github.com>
2023-03-16 16:56:34 +0300

    Update events combinations (beta).py

--- events combinations (beta).py
+++ events combinations (beta).py
@@ -1,11 +1,9 @@
-import math
-
 events = int(input('\nAmount of events: '))
 varieties = int(input('Amount of results in each event: '))
 bet = int(input('Bet amount: '))
 currency = str(input('Write your currency: '))
 
-combinations = math.factorial(events) // (math.factorial(events - varieties) * math.factorial(varieties))
+combinations = events * pow(varieties, events)
 
 print('\nAmount of combinations: ', combinations)
 print('\nRequired amount: ', combinations * bet, currency)
